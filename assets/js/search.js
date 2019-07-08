8179168884020194885519812befd20ec1e09ae5
Antón Larrazabal <alarrazabal@systemsgroup.es>
2019-07-08 13:39:37 +0200

    cambios sidebar busqueda

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,5 +2,7 @@ SimpleJekyllSearch({
   searchInput: document.getElementById('search-input'),
   resultsContainer: document.getElementById('results-container'),
   limit: 10,
-  json: '/search.json'
+  json: '/search.json',
+  noResultsText: 'No hay resultados',
+  searchResultTemplate: '<li><a href="{url}"><img src="{thumbnail}"/><span>{title}</span></a></li>'
 })
