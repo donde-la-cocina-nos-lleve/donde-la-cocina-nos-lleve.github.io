f87b64df402454f11cbb98c9d67ad1582a670930
AntonLH <anton.larrahi@gmail.com>
2019-07-20 13:59:47 +0200

    cambios js search filter desktop

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,13 +1,14 @@
 SimpleJekyllSearch({
   searchInput: document.getElementById('search-input'),
   resultsContainer: document.getElementById('results-container'),
-  limit: 100,
+  limit: 30,
   json: '/search.json',
   noResultsText: 'No hay resultados',
   searchResultTemplate: '<li class="objeto_resultado show {category}"><a href="{url}"><img src="{thumbnail}"/><p>{title}</p></a></li>'
 })
+var btn_all;
 document.addEventListener("DOMContentLoaded", function() {
-    var btn_all = document.getElementById("search-container").getElementsByClassName("active")[0];
+    btn_all = document.getElementById("search-container").getElementsByClassName("active")[0];
     filter(btn_all, "all");
 });
 var filtro="";
@@ -21,31 +22,27 @@ var input=document.getElementById("search-input").addEventListener('keyup', func
 function filter(elem, param_filtro) {
   var resultados;
   resultados = document.getElementsByClassName("objeto_resultado");
-  console.log(param_filtro=="all");
-  console.log(param_filtro);
-  console.log(filtro);
   last_param_filtro=param_filtro;
   if (param_filtro == "all"){
     filtro = "";
     elem=btn_all;
     if(elem.className.indexOf("active")==-1){
       var btnContainer = document.getElementById("search-container");
-      var actives = btnContainer.getElementsByClassName("active");
-      console.log(actives.length);
-      for (var i = 0; i < actives.length; i++) {
-        actives[i].className=delete_from_array(actives[i].className.split(" "), "active"); 
-      }
-      elem.className += " active";
+      var elems = btnContainer.querySelectorAll(".active");
+      [].forEach.call(elems, function(el) {
+          el.classList.remove("active");
+      });
+      elem.classList.add("active");
     }
   }
   else{
     if(elem != null){
       if(elem.className.indexOf("active")>-1){
-        elem.className=delete_from_array(elem.className.split(" "), "active");
+        elem.classList.remove("active");
       }
       else{
-        btn_all.className=delete_from_array(btn_all.className.split(" "), "active");
-        elem.className += " active";
+        btn_all.classList.remove("active");
+        elem.classList.add("active");
       }
       if(filtro.indexOf(param_filtro)>-1){
         filtro=delete_from_comma_array(filtro.split(","),param_filtro);
@@ -68,15 +65,11 @@ function filter(elem, param_filtro) {
 }
 
 function show(element) {
-  var arr1 = element.className.split(" ");
-  if (arr1.indexOf("show") == -1) {
-      element.className += " show";
-  }
+  element.classList.add("show");
 }
 
 function hide(element) {
-  var arr1 = element.className.split(" ");
-  element.className = delete_from_array(arr1, "show");
+  element.classList.remove("show");
 }
 
 function delete_from_comma_array(arr, str){
@@ -84,12 +77,5 @@ function delete_from_comma_array(arr, str){
       arr.splice(arr.indexOf(str), 1); 
   }
   return arr.join(",");
-}
-
-function delete_from_array(arr, str){
-  while (arr.indexOf(str) > -1) {
-      arr.splice(arr.indexOf(str), 1); 
-  }
-  return arr.join(" ");
 }
 
