043d980dbd200dc19453eacc6e41c189946dbe3b
AntonLH <anton.larrahi@gmail.com>
2020-03-04 16:27:32 +0100

    fix sw & instagram img

--- sw.js
+++ sw.js
@@ -82,24 +82,22 @@ self.addEventListener('push', ev => {
 });
 
 self.addEventListener('notificationclick', function(event) {
-    let url = event.notification.data.url;
-    else{
-        event.notification.close(); // Android needs explicit close.
-        event.waitUntil(
-            clients.matchAll({type: 'window'}).then( windowClients => {
-                // Check if there is already a window/tab open with the target URL
-                for (var i = 0; i < windowClients.length; i++) {
-                    var client = windowClients[i];
-                    // If so, just focus it.
-                    if (client.url === url && 'focus' in client) {
-                        return client.focus();
-                    }
-                }
-                // If not, then open the target URL in a new window/tab.
-                if (clients.openWindow) {
-                    return clients.openWindow(url);
-                }
-            })
-        );
-    }
+	let url = event.notification.data.url;
+	event.notification.close(); // Android needs explicit close.
+	event.waitUntil(
+		clients.matchAll({type: 'window'}).then( windowClients => {
+			// Check if there is already a window/tab open with the target URL
+			for (var i = 0; i < windowClients.length; i++) {
+				var client = windowClients[i];
+				// If so, just focus it.
+				if (client.url === url && 'focus' in client) {
+					return client.focus();
+				}
+			}
+			// If not, then open the target URL in a new window/tab.
+			if (clients.openWindow) {
+				return clients.openWindow(url);
+			}
+		})
+	);
 });
